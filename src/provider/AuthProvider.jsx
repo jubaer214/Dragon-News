49610aa2540a2b06967e6c44c80a19b2c6f712dc
Jubaer Ahmad <ielahi61@gmail.com>
2025-10-29 18:43:22 +0600

    loading

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -13,21 +13,24 @@ const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
   console.log(user);
   const createUser = (email, password) => {
+    setLoading(true)
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
-
-    const signIn = (email, password) => {
-        return signInWithEmailAndPassword(auth, email, password)
-    }
+  const signIn = (email, password) => {
+    setLoading(true)
+    return signInWithEmailAndPassword(auth, email, password);
+  };
   const logOut = () => {
     return signOut(auth);
   };
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
+      setLoading(false);
     });
     return () => {
       unsubscribe();
@@ -40,6 +43,8 @@ const AuthProvider = ({ children }) => {
     createUser,
     logOut,
     signIn,
+    loading,
+    setLoading,
   };
   return <AuthContext value={authData}>{children}</AuthContext>;
 };
--- src/provider/PrivateRoute.jsx
+++ src/provider/PrivateRoute.jsx
@@ -3,8 +3,15 @@ import { AuthContext } from './AuthProvider';
 import { Navigate } from 'react-router';
 
 const PrivateRoute = ({ children }) => {
-    const { user } = use(AuthContext);
+    const { user, loading } = use(AuthContext);
 
+    if (loading) {
+        return (
+          <div className='flex justify-center min-h-screen items-center'>
+            <span className="loading loading-ring loading-xl"></span>
+          </div>
+        );
+    }
     if (user && user?.email) {
         return children
     } else {
